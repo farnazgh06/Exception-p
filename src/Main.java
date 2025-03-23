e76c76e08d69afab0e99268d4a981b72838642e4
farnaz <whoisfarnaz@gmail.com>
2025-03-24 00:04:26 +0330

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Library library = new Library();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            library.listBooks();
+        } catch (EmptyLibraryException e) {
+            System.out.println("Library id empty");
+        }
+
+        Book book1 = null;
+        Book book2 = null;
+        Book book3 = null;
+        Book book4 = null;
+
+
+        try {
+            book1 = new Book("Java Programming", 300);
+            book2 = new Book("Design Patterns", -3);
+            book3 = new Book(null, 100);
+            book4 = new Book("", 400);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Title cannot be empty or Page count must be positive");
+        }
+
+
+        try {
+            library.addBook(book1);
+            library.addBook(book2);
+            library.addBook(book3);
+            library.addBook(book4);
+
+        } catch (InvalidBookException e) {
+            System.out.println("Book should not be null");
+        }
+
+
+
+        try {
+            library.listBooks();
+        } catch (EmptyLibraryException e) {
+            System.out.println("Library id empty");
+        }
+
+
+        try {
+            library.borrowBook("Java Programming");
+            library.returnBook("Clean Code");
+        }
+        catch (EmptyLibraryException | BookNotFoundException e) {
+            System.out.println("Library is empty, no books to borrow.");
+
+        }
+
+
+        try {
+            library.returnBook("Java Programming");
+        } catch (BookNotFoundException e) {
+            System.out.println ("Cannot return.");
+
         }
     }
+
 }
